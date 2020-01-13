d2edc47079bfe812ae8d34e3fa19d921636cd350
Dexter Leng <34204380+dexterleng@users.noreply.github.com>
2020-01-13 18:46:37 +0800

    Save top n file compare results (#21)
    * Add tests for match density and line match count
    
    * Save top k file compare results

--- start.ml
+++ start.ml
@@ -1,5 +1,8 @@
 open Core
 open Project_compare
+          
+type list_of_file_compare_results = file_compare_result list [@@deriving yojson]
+
 
 let command =
   Command.basic
@@ -17,14 +20,27 @@ let command =
           Out_channel.write_all (Filename.concat output_dir "README.md") ~data: "This is a file created so we do not do a huge computation and find out you can't save in the output directory.";
           let projects = build_projects projects_parent_dir in
           print_endline "Projects have been generated and kgrams have been selected. Performing comparisons.";
-          let all_compare_result = compare_all_projects projects in
-          print_endline "";
-          List.iter ~f:(fun result_thunk ->
-            let r = result_thunk () in
-            let filename = Printf.sprintf "%s_%s.json" r.project_a.project_name r.project_b.project_name in
-            let file_dir = Filename.concat output_dir filename in
-            Yojson.Safe.to_file file_dir (project_compare_result_to_yojson r);
-          ) all_compare_result
+          let project_compare_result_thunks = compare_all_projects projects in
+          let top_file_compare_results = top_k_file_compare_results project_compare_result_thunks ~k:1000 in
+          
+          (* save top k file compare results, where the file compare with the highest line count match is 1.json *)
+          List.iteri top_file_compare_results ~f:(fun i r -> 
+              let filename = Printf.sprintf "%d.json" i in
+              let file_dir = Filename.concat output_dir filename in
+              Yojson.Safe.to_file file_dir (file_compare_result_to_yojson { r with
+                project_a_file = { r.project_a_file with selected_kgrams_by_hash = Hashtbl.create (module Int); };
+                project_b_file = { r.project_b_file with selected_kgrams_by_hash = Hashtbl.create (module Int); };
+              });
+          );
+
+          (* we need to save the top k in a single file for displaying a table so this is a lightweight version without kgrams *)
+          let top_k_lightweight = List.map top_file_compare_results ~f:(fun r -> { r with
+            matching_kgrams = [];
+            project_a_file = { r.project_a_file with selected_kgrams_by_hash = Hashtbl.create (module Int); };
+            project_b_file = { r.project_b_file with selected_kgrams_by_hash = Hashtbl.create (module Int); };
+          }) in
+
+          Yojson.Safe.to_file (Filename.concat output_dir "summary.json") (list_of_file_compare_results_to_yojson top_k_lightweight);
        )))
 
 let () =
---
+++ test_project_compare.ml
@@ -0,0 +1,69 @@
+open OUnit2
+open Core
+open Project_compare
+open Preprocessing
+
+let kgram1 = {
+    hash = 10;
+    length = 20;
+    lines_occupied = 1;
+    starting_line = 1;
+    starting_index_in_line = 5;
+}
+
+let kgram2 = {
+    hash = 999;
+    length = 20;
+    lines_occupied = 1;
+    starting_line = 1;
+    starting_index_in_line = 5;
+}
+
+let kgram3 = {
+    hash = 123;
+    length = 20;
+    lines_occupied = 1;
+    starting_line = 1;
+    starting_index_in_line = 5;
+}
+
+let tests = [
+  "generate_range_set" >:: (fun _ -> assert_equal ~cmp:(Set.equal)
+      (Set.of_list (module Int) [1;2;3;4;5;]) 
+      (generate_range_set 1 5)
+  );
+
+  "calculate_match_density" >:: (fun _ -> assert_equal
+    ((1.0, 1.0))
+    (calculate_match_density (build_kgram_by_hash_map [kgram1; kgram1; kgram1;]) (build_kgram_by_hash_map [kgram1; kgram1; kgram1;]))
+  );
+  "calculate_match_density2" >:: (fun _ -> assert_equal
+    ((1.0 /. 3.0, 1.0))
+    (calculate_match_density (build_kgram_by_hash_map [kgram1; kgram2; kgram3;]) (build_kgram_by_hash_map [kgram1; kgram1; kgram1;]))
+  );
+  "calculate_match_density3" >:: (fun _ -> assert_equal
+    ((0.0, 0.0))
+    (calculate_match_density (build_kgram_by_hash_map [kgram1; kgram1; kgram1;]) (build_kgram_by_hash_map [kgram2; kgram2; kgram2;]))
+  );
+  "calculate_match_density4" >:: (fun _ -> assert_equal
+    ((0.5, 2.0 /. 3.0))
+    (calculate_match_density (build_kgram_by_hash_map [kgram1; kgram2;]) (build_kgram_by_hash_map [kgram2; kgram2; kgram3;]))
+  );
+
+  "calculate_line_matches" >:: (fun _ -> assert_equal
+    (1, 1)
+    (calculate_line_matches (build_kgram_by_hash_map [kgram1; kgram1;]) (build_kgram_by_hash_map [kgram1; kgram1;]))
+  );
+  "calculate_line_matches2" >:: (fun _ -> assert_equal
+    (1, 10)
+    (calculate_line_matches (build_kgram_by_hash_map [kgram3;]) (build_kgram_by_hash_map [{ kgram3 with lines_occupied = 10;  };]))
+  );
+  "calculate_line_matches3" >:: (fun _ -> assert_equal
+    (1, 10)
+    (calculate_line_matches (build_kgram_by_hash_map [kgram3;]) (build_kgram_by_hash_map [kgram3; { kgram3 with lines_occupied = 10 };]))
+  );
+  "calculate_line_matches4" >:: (fun _ -> assert_equal
+    (1, 20)
+    (calculate_line_matches (build_kgram_by_hash_map [kgram3;]) (build_kgram_by_hash_map [{ kgram3 with starting_line = 100; lines_occupied = 10; }; { kgram3 with lines_occupied = 10 };]))
+  );
+]
