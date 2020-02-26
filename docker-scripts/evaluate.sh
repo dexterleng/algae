8c8efd472d9f93e58387381921fd892e4fa2fb16
Dexter Leng <dexterlengcs@gmail.com>
2020-02-26 15:57:06 +0800

    Unzip projects in docker start script

--- docker-scripts/evaluate.sh
+++ docker-scripts/evaluate.sh
@@ -7,5 +7,9 @@ if [ -f "already_ran" ]; then
 fi
 sudo touch already_ran
 
+cd ./projects/
+for i in *.zip; do sudo unzip "$i" -d "${i%%.zip}"; done
+cd ../
+
 sudo mkdir ./compare_results/
 sudo ./_build/default/start.exe -k $K -w $W -m $M -output-dir ./compare_results/ -projects-dir ./projects/ -file-type js -ignore-dir node_modules
